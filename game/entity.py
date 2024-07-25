b01e92fb3ac5b349b89cd08cac572b2519933dbf
Leseptum <o-engelhardt@web.de>
2024-07-25 13:23:30 +0200

    Antalia 0.1

--- game/entity.py
+++ game/entity.py
@@ -1,27 +1,82 @@
+import json
+
 class Player:
-    def __init__(self,strength,dexterity,intelligence,endurance,charisma,willpower):
+
+    def createStats(self,name,strength,dexterity,intelligence,constitution,charisma,willpower,temphealth,xp,loc,lmid):
+        self.name = name
         self.strength = strength
         self.dexterity = dexterity
         self.intelligence = intelligence
-        self.endurance = endurance
+        self.constitution = constitution
         self.charisma = charisma
         self.willpower= willpower
-        self.health = (strength + endurance)
+        self.maxhealth = (strength + constitution)
+        self.temphealth = temphealth
         self.melee = (strength + dexterity) / 2
         self.ranged = (dexterity + intelligence) / 2
-        self.defense = (endurance + willpower) / 2
+        self.defense = (constitution + willpower) / 2
         self.magic = (intelligence + willpower) / 2
-        self.xp = 0
-        self.location = None
-
+        self.xp = xp
+        self.location = loc
+        self.lastMessageID = lmid
+        
     def getStats(self):
-        return {"Stärke":self.strength,"Geschicklichkeit":self.dexterity,"Intelligenz":self.intelligence,"Ausdauer":self.endurance,"Charisma":self.charisma,"Willenskraft":self.willpower,"Leben":self.health,"Nahkampf":self.melee,"Fernkampf":self.ranged, "Verteidigung":self.defense, "Magic":self.magic,"XP":self.xp,"Ort":self.location}
+        return {
+            "name":self.name,
+            "Str":self.strength,
+            "Dex":self.dexterity,
+            "Int":self.intelligence,
+            "Con":self.constitution,
+            "Cha":self.charisma,
+            "Wil":self.willpower,
+            "tempHP":self.temphealth,
+            "XP":self.xp,
+            "Loc":self.location,
+            "lmid":self.lastMessageID
+            }
 
-    def getHurt(self,damage):
+    def __init__(self,ID):
+        self.ID = ID
+
+        try:
+            #load existing data
+            tempData = json.loads(open(f"playerData/{self.ID}.json", "r").read())
+            self.createStats(tempData["name"],
+                             tempData["Str"],
+                             tempData["Dex"],
+                             tempData["Int"],
+                             tempData["Con"],
+                             tempData["Cha"],
+                             tempData["Wil"],
+                             tempData["tempHP"],
+                             tempData["XP"],
+                            tempData["Loc"],
+                            tempData["lmid"])
+        except:
+            print("load playerdata : failed\nusing default")
+            self.createStats(None,10,10,10,10,10,10,10,None,None,None)
+            
+            
+            # muss Antwortconzept ausdenken. mabye mit erwarteAntwort = True/False
+
+    def saveData(self):
+        player = (open(f"playerData/{self.ID}.json", "w"))
+        writeOutdata = self.getStats()
+        player.write(json.dumps(writeOutdata, indent=4))
+        player.close()
+
+    def decreaseHP(self,damage):
         damage=damage-self.defense
         if damage > 0:
             return self.health-damage
         elif damage <= 0:
             return self.health
 
-    
+
+
+'''
+PLAYER-ID-SEARCH
+test = Player(input("playerID:"))
+print(test.getStats())
+test.saveData()
+'''
--- game/gamelogic.py
+++ game/gamelogic.py
@@ -1,17 +1,19 @@
 import entity,json
 
-player=open("player.json","r")
-print(player.read())
+storydata = json.loads(open(f"game/text.json", "r").read())
 
 
-'''
 def getText(message):
-    if message.content == "$play":
-        if str(message.author.id) in player:return "Hello Player"
-        else:
-            player[str(message.author.id)] = ""
-            textfile.write(json.dumps(player))
-            return "Welcome"
-
-
-'''
+    player=entity.Player(message.author.id)
+    player.saveData()    
+    if player.lastMessageID == None:
+        player.lastMessageID = 1
+        player.saveData()
+        return "Wie heißt du?"
+    if player.lastMessageID == 1:
+        player.name = message.content[1:]
+        player.lastMessageID = 2
+        player.saveData()
+        return f"hello {player.name}"
+    if player.lastMessageID == 2:
+        return f"{player.getStats()}"
